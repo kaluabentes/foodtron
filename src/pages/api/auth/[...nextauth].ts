2e7288a62993090dfb1ba2387c796e6456907f32
Kaluã Bentes <kaluanbentes@gmail.com>
2023-02-02 17:37:08 -0400

    refactor: add email function

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import sendVerificationRequest from "@/lib/infra/nextAuth/sendVerificationRequest"
 
 import prisma from "@/lib/infra/prisma/client"
 
@@ -11,13 +12,14 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: Number(process.env.EMAIL_SERVER_PORT),
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD,
         },
       },
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
