b1075e8cc9dce5ce6afb77a4dcf419dad0c856ee
Kaluã Bentes <kaluanbentes@gmail.com>
2023-02-16 22:58:10 -0400

    fix: remove height

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -7,26 +7,23 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout = ({ children, isLarge = false }: AuthLayoutProps) => (
-  <Box height="100vh" overflowY="auto">
-    <Flex
-      justifyContent="center"
-      paddingTop={{ base: 0, sm: 10 }}
-      paddingBottom={{ base: 0, sm: 10 }}
+  <Flex
+    justifyContent="center"
+    paddingTop={{ base: 0, sm: 10 }}
+    paddingBottom={{ base: 0, sm: 10 }}
+  >
+    <Box
+      overflowY={{ base: "auto", sm: "initial" }}
+      width="100%"
+      maxWidth={isLarge ? "600px" : "448px"}
+      backgroundColor={useColorModeValue("white", "gray.800")}
+      padding={{ base: 5, sm: 10 }}
+      boxShadow={{ base: "none", sm: useColorModeValue("md", "md-dark") }}
+      borderRadius={{ base: "none", sm: 10 }}
     >
-      <Box
-        height={{ base: "100vh", sm: "initial" }}
-        overflowY={{ base: "auto", sm: "initial" }}
-        width="100%"
-        maxWidth={isLarge ? "600px" : "448px"}
-        backgroundColor={useColorModeValue("white", "gray.800")}
-        padding={{ base: 5, sm: 10 }}
-        boxShadow={{ base: "none", sm: useColorModeValue("md", "md-dark") }}
-        borderRadius={{ base: "none", sm: 10 }}
-      >
-        {children}
-      </Box>
-    </Flex>
-  </Box>
+      {children}
+    </Box>
+  </Flex>
 )
 
 export default AuthLayout
