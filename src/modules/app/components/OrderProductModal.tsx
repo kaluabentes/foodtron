9ae566f7f59dad33cc73edb6e51ba303ab771c97
Kaluã Bentes <kaluanbentes@gmail.com>
2023-02-17 20:38:27 -0400

    refactor: rollback changes

--- src/modules/app/components/OrderProductModal.tsx
+++ src/modules/app/components/OrderProductModal.tsx
@@ -229,7 +229,7 @@ const OrderProductModal = ({
             <Text mb={2} color="gray.500" fontSize="sm">
               {product?.description}
             </Text>
-            <Text fontWeight="500" fontSize="sm">
+            <Text fontWeight="500" fontSize="md">
               {formatToRealCurrency(Number(product?.price))}
             </Text>
           </Box>
