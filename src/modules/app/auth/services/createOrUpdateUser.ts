9c20d37ca4b135d836e95e577a145eb1bb22767e
Kaluã Bentes <kaluanbentes@gmail.com>
2023-04-05 18:28:03 -0400

    refactor: extract creation logic

---
+++ src/modules/app/auth/services/createOrUpdateUser.ts
@@ -0,0 +1,105 @@
+import prisma from "@/lib/infra/prisma/client"
+import Address from "@/modules/app/addresses/types/Address"
+import Order from "@/modules/admin/orders/types/Order"
+
+const createOrUpdateUser = async (body: any) => {
+  const store = await prisma.store.findFirst({
+    where: {
+      subdomain: body.subdomain,
+    },
+  })
+
+  const include = {
+    orders: {
+      include: {
+        user: true,
+        orderProducts: {
+          include: {
+            options: true,
+          },
+        },
+      },
+    },
+    addresses: {
+      include: {
+        location: true,
+      },
+    },
+  }
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      storeId: store?.id,
+      phone: body.phone,
+    },
+    include,
+  })
+
+  if (existingUser) {
+    const user = await prisma.user.update({
+      where: {
+        id: existingUser.id,
+      },
+      data: {
+        selectedAddressId: body.selectedAddressId,
+        orders: {
+          connect: body.orders.map((order: Order) => ({
+            id: order.id,
+          })),
+        },
+        addresses: {
+          create: body.addresses.map((address: Address) => ({
+            street: address.street,
+            number: address.number,
+            latitude: address.latitude,
+            longitude: address.longitude,
+            location: {
+              connect: {
+                id: address.location.id,
+              },
+            },
+          })),
+        },
+      },
+      include,
+    })
+
+    return user
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name: body.name,
+      phone: body.phone,
+      selectedAddressId: body.selectedAddressId,
+      store: {
+        connect: {
+          id: store?.id,
+        },
+      },
+      orders: {
+        connect: body.orders.map((order: Order) => ({
+          id: order.id,
+        })),
+      },
+      addresses: {
+        create: body.addresses.map((address: Address) => ({
+          street: address.street,
+          number: address.number,
+          latitude: address.latitude,
+          longitude: address.longitude,
+          location: {
+            connect: {
+              id: address.location.id,
+            },
+          },
+        })),
+      },
+    },
+    include,
+  })
+
+  return user
+}
+
+export default createOrUpdateUser
--- src/pages/api/auth/verify-code.ts
+++ src/pages/api/auth/verify-code.ts
@@ -2,24 +2,23 @@ import { NextApiRequest, NextApiResponse } from "next"
 import NextCors from "nextjs-cors"
 import jwt from "jsonwebtoken"
 
-import sendVerification from "@/lib/infra/sinch/sendVerification"
 import verifyCode from "@/lib/infra/sinch/verifyCode"
 import prisma from "@/lib/infra/prisma/client"
 import Address from "@/modules/app/addresses/types/Address"
 import Order from "@/modules/admin/orders/types/Order"
+import createOrUpdateUser from "@/modules/app/auth/services/createOrUpdateUser"
 
 const ALLOWED_METHODS = ["POST"]
 
 const verifyCodeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   await NextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    methods: ALLOWED_METHODS,
     origin: "*",
     optionsSuccessStatus: 200,
   })
 
   if (!ALLOWED_METHODS.includes(req.method!)) {
-    return res.status(400).send("Method not allowed")
+    return res.status(400).send(`Method not allowed`)
   }
 
   if (!req.body.subdomain) {
@@ -46,107 +45,7 @@ const verifyCodeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     const response = await verifyCode(req.body.phone, req.body.code)
 
     if (response.data.status === "SUCCESSFUL") {
-      const store = await prisma.store.findFirst({
-        where: {
-          subdomain: req.body.subdomain,
-        },
-      })
-
-      const include = {
-        orders: {
-          include: {
-            user: true,
-            orderProducts: {
-              include: {
-                options: true,
-              },
-            },
-          },
-        },
-        addresses: {
-          include: {
-            location: true,
-          },
-        },
-      }
-
-      const existingUser = await prisma.user.findFirst({
-        where: {
-          storeId: store?.id,
-          phone: req.body.phone,
-        },
-        include,
-      })
-
-      if (existingUser) {
-        const token = await jwt.sign(existingUser, process.env.JWT_SECRET!)
-
-        const user = await prisma.user.update({
-          where: {
-            id: existingUser.id,
-          },
-          data: {
-            selectedAddressId: req.body.selectedAddressId,
-            orders: {
-              connect: req.body.orders.map((order: Order) => ({
-                id: order.id,
-              })),
-            },
-            addresses: {
-              create: req.body.addresses.map((address: Address) => ({
-                street: address.street,
-                number: address.number,
-                latitude: address.latitude,
-                longitude: address.longitude,
-                location: {
-                  connect: {
-                    id: address.location.id,
-                  },
-                },
-              })),
-            },
-          },
-          include,
-        })
-
-        return res.send({
-          token,
-          user,
-        })
-      }
-
-      const user = await prisma.user.create({
-        data: {
-          name: req.body.name,
-          phone: req.body.phone,
-          selectedAddressId: req.body.selectedAddressId,
-          store: {
-            connect: {
-              id: store?.id,
-            },
-          },
-          orders: {
-            connect: req.body.orders.map((order: Order) => ({
-              id: order.id,
-            })),
-          },
-          addresses: {
-            create: req.body.addresses.map((address: Address) => ({
-              street: address.street,
-              number: address.number,
-              latitude: address.latitude,
-              longitude: address.longitude,
-              location: {
-                connect: {
-                  id: address.location.id,
-                },
-              },
-            })),
-          },
-        },
-        include,
-      })
-
+      const user = await createOrUpdateUser(req.body)
       const token = await jwt.sign(user, process.env.JWT_SECRET!)
 
       return res.send({
