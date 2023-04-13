0af91844e185075269e431cc9984086c407e9d6b
Kaluã Bentes <kaluanbentes@gmail.com>
2023-04-13 00:49:00 -0400

    refact: add overlay

--- src/components/AppBar/SideNav.tsx
+++ src/components/AppBar/SideNav.tsx
@@ -1,4 +1,4 @@
-import { CloseButton, Flex } from "@chakra-ui/react"
+import { Box, CloseButton, Flex, ModalOverlay } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 
 import Menu from "@/components/Menu"
@@ -27,68 +27,82 @@ const SideNav = ({
 
   return (
     <ClickOutside onClickOutside={onClose}>
-      <Flex
-        as="nav"
-        direction="column"
-        flexGrow="1"
-        width="100%"
-        maxWidth="220px"
-        padding={3}
-        backgroundColor="white"
-        height="100%"
-        transition="0.3s"
-        position="fixed"
-        top="0"
-        left={isOpen ? "0" : "-220px"}
-        visibility={isOpen ? "visible" : "hidden"}
-        shadow="md"
-        zIndex={200}
-      >
-        <CloseButton
+      <Box position="relative">
+        <Box
+          as="button"
           onClick={onClose}
-          position="absolute"
-          top="17px"
-          right="-15px"
-          backgroundColor="white"
-          borderRadius="50%"
-          boxShadow="md"
-          _hover={{ backgroundColor: "gray.100" }}
-          _active={{ backgroundColor: "gray.100" }}
+          visibility={isOpen ? "visible" : "hidden"}
+          opacity={isOpen ? "1" : "0"}
+          transition="0.5s"
+          background="rgba(0, 0, 0, 0.5)"
+          position="fixed"
+          height="100%"
+          width="100%"
+          zIndex={150}
         />
-        {header}
-        <Flex direction="column" flexGrow={1} justifyContent="space-between">
-          <Menu>
-            {topMenu.map((item: any, index) => (
-              <MenuItem
-                key={String(index + 1)}
-                onClick={() =>
-                  item.onClick ? item.onClick() : router.push(item.path)
-                }
-                icon={item.icon}
-                isLight
-                isActive={router.asPath === item.path}
-              >
-                {item.label}
-              </MenuItem>
-            ))}
-          </Menu>
-          <Menu>
-            {bottomMenu.map((item: any, index) => (
-              <MenuItem
-                key={String(index + 1)}
-                onClick={() =>
-                  item.onClick ? item.onClick() : router.push(item.path)
-                }
-                icon={item.icon}
-                isActive={router.asPath === item.path}
-                isLight
-              >
-                {item.label}
-              </MenuItem>
-            ))}
-          </Menu>
+        <Flex
+          as="nav"
+          direction="column"
+          flexGrow="1"
+          width="100%"
+          maxWidth="220px"
+          padding={3}
+          backgroundColor="white"
+          height="100%"
+          transition="0.3s"
+          position="fixed"
+          top="0"
+          left={isOpen ? "0" : "-220px"}
+          visibility={isOpen ? "visible" : "hidden"}
+          shadow="md"
+          zIndex={200}
+        >
+          <CloseButton
+            onClick={onClose}
+            position="absolute"
+            top="17px"
+            right="-15px"
+            backgroundColor="white"
+            borderRadius="50%"
+            boxShadow="md"
+            _hover={{ backgroundColor: "gray.100" }}
+            _active={{ backgroundColor: "gray.100" }}
+          />
+          {header}
+          <Flex direction="column" flexGrow={1} justifyContent="space-between">
+            <Menu>
+              {topMenu.map((item: any, index) => (
+                <MenuItem
+                  key={String(index + 1)}
+                  onClick={() =>
+                    item.onClick ? item.onClick() : router.push(item.path)
+                  }
+                  icon={item.icon}
+                  isLight
+                  isActive={router.asPath === item.path}
+                >
+                  {item.label}
+                </MenuItem>
+              ))}
+            </Menu>
+            <Menu>
+              {bottomMenu.map((item: any, index) => (
+                <MenuItem
+                  key={String(index + 1)}
+                  onClick={() =>
+                    item.onClick ? item.onClick() : router.push(item.path)
+                  }
+                  icon={item.icon}
+                  isActive={router.asPath === item.path}
+                  isLight
+                >
+                  {item.label}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Flex>
         </Flex>
-      </Flex>
+      </Box>
     </ClickOutside>
   )
 }
